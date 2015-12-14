57941c529696c09fabfb7d5a77b93ef8863b6b45
Ben Johnson <bhjohnson@bhjohnson-4819.local>
2015-12-14 13:03:27 -0800

    Added mktorest file, which is currently incomplete

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/index')
 def index():
     return "Welcome to the MarketoLive Server!"
+
+# @app.route('/robots.txt')
+# def sendrobot():
+# 	return app.send_static_file('robots.txt')
