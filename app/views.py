819bbc846883cb4307e1b4cbfe87b5aa0952737a
cjhickens <Carter@Carters-MacBook-Pro.local>
2015-12-23 11:34:07 -0800

    fix views

--- app/views.py
+++ app/views.py
@@ -50,52 +50,52 @@ class CreateFolders(Resource):
 api.add_resource(CreateFolders, '/createfolders/<string:api_key_in>/<string:new_email>')
 
 
-rl_parser = reqparse.RequestParser()
-rl_parser.add_argument('firstName')
-rl_parser.add_argument('lastName')
-rl_parser.add_argument('email', required=True)
-rl_parser.add_argument('accountString', required=True)
-rl_parser.add_argument('pod',required=True)
-rl_parser.add_argument('loginDate')
+# rl_parser = reqparse.RequestParser()
+# rl_parser.add_argument('firstName')
+# rl_parser.add_argument('lastName')
+# rl_parser.add_argument('email', required=True)
+# rl_parser.add_argument('accountString', required=True)
+# rl_parser.add_argument('pod',required=True)
+# rl_parser.add_argument('loginDate')
 
-#Endpoint to track who is using mktolive - pass in first/last/email-of-user-id/account-string/pod/(I infer current login date or accept login date)
-#may be taking in marketo munchkin ID and not pod, but need one of the two
-class RecordLogin(Resource):
-	def post(self, api_key_in):
-		args=rl_parser.parse_args()
-		if 'loginDate' not in args:
-			login_date=datetime.utcnow()
-		else:
-			login_date=datetime.fromtimestamp(args['loginDate'], timezone.utc)
-		user = models.User.query.filter_by(email=args['email']).first()
-		if not user:
-			user = models.User()
-			user.first_name = args['firstName']
-			user.last_name = args['lastName']
-			user.email = args['email']
-			#eventually: here we will query the marketo instance with the user db to populate the rest of the fields
-		sub = user.subscriptions.filter_by(account_string=args['accountString']).first()
-		if not sub:
-			sub = models.Subscription()
-			sub.mkto_pod = args['pod']
-			sub.account_string = args['accountString']
+# #Endpoint to track who is using mktolive - pass in first/last/email-of-user-id/account-string/pod/(I infer current login date or accept login date)
+# #may be taking in marketo munchkin ID and not pod, but need one of the two
+# class RecordLogin(Resource):
+# 	def post(self, api_key_in):
+# 		args=rl_parser.parse_args()
+# 		if 'loginDate' not in args:
+# 			login_date=datetime.utcnow()
+# 		else:
+# 			login_date=datetime.fromtimestamp(args['loginDate'], timezone.utc)
+# 		user = models.User.query.filter_by(email=args['email']).first()
+# 		if not user:
+# 			user = models.User()
+# 			user.first_name = args['firstName']
+# 			user.last_name = args['lastName']
+# 			user.email = args['email']
+# 			#eventually: here we will query the marketo instance with the user db to populate the rest of the fields
+# 		sub = user.subscriptions.filter_by(account_string=args['accountString']).first()
+# 		if not sub:
+# 			sub = models.Subscription()
+# 			sub.mkto_pod = args['pod']
+# 			sub.account_string = args['accountString']
 
-		sub.last_login = login_date
-account_string = db.Column(db.String(80))
-    mkto_pod = db.Column(db.String(20))
-    login = db.Column(db.String(64))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    is_admin = db.Column(db.Boolean)
-    last_login = db.Column(db.DateTime)
+# 		sub.last_login = login_date
+# account_string = db.Column(db.String(80))
+#     mkto_pod = db.Column(db.String(20))
+#     login = db.Column(db.String(64))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     is_admin = db.Column(db.Boolean)
+#     last_login = db.Column(db.DateTime)
 
-first_name = db.Column(db.String(32))
-    last_name = db.Column(db.String(32))
-    email = db.Column(db.String(64))
-    role = db.Column(db.String(20))
-    password = db.Column(db.String(80))
-    marketo_lead_id = db.Column(db.Integer)
-    created = db.Column(db.DateTime)
-    subscriptions
+# first_name = db.Column(db.String(32))
+#     last_name = db.Column(db.String(32))
+#     email = db.Column(db.String(64))
+#     role = db.Column(db.String(20))
+#     password = db.Column(db.String(80))
+#     marketo_lead_id = db.Column(db.Integer)
+#     created = db.Column(db.DateTime)
+#     subscriptions
 
 
 # This was an example for pope on how to serve robots.txt, we may use it later
