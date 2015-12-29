ac77740de4d04ad30f37d8f26e0ccca48ee8e22c
Chris Pope <cpope13@gmail.com>
2015-12-28 18:07:59 -0800

    Add help icon to nav in base.html

--- app/static/base.js
+++ app/static/base.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('.logo-nav-container').hover(function(){
     $('.logo-nav-container').toggle();
   });
+  $('.help-nav-container').hover(function(){
+    $('.help-nav-container').toggle();
+  });
 });
