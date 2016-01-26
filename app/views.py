1f375be6d2a69d80c87ee5531bcde5f246286909
cjhickens <Carter@Carters-MacBook-Pro.local>
2016-01-26 08:33:25 -0800

    continued to build out higher ed page

--- app/views.py
+++ app/views.py
@@ -70,7 +70,7 @@ def login():
 #                            providers=app.config['OPENID_PROVIDERS'])
 
 languages = ['en', 'jp']
-pages = ['base', 'b2b', 'lead-management']
+pages = ['base', 'b2b', 'lead-management', 'higher-ed']
 
 @app.route('/')
 def no_language():
@@ -102,9 +102,11 @@ def feature_function(language):
 		return redirect('/en/lead-management.html')
 	return render_template(language+'/lead-management.html', form=g.loginform, name=g.name, lang=language, page='lead-management')
 	
-@app.route('/higher-ed')
-def higher_ed():
-	return render_template('higher-ed.html', form=g.loginform, name=g.name)
+@app.route('/<language>/higher-ed')
+def higher_ed(language):
+	if language not in languages:
+		return redirect('/en/higher-ed.html')
+	return render_template(language+'/higher-ed.html', form=g.loginform, name=g.name)
 
 
 
