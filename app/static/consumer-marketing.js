1a5e2274d5c47fa671341e8fe29eab65c26bc603
Chris Pope <cpope13@gmail.com>
2016-01-29 17:53:29 -0800

    slight css modifications to consumer-marketing

--- app/static/consumer-marketing.js
+++ app/static/consumer-marketing.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
   $('.circle p').hover(function(){
     $(this).siblings('div').children('img').toggle();
   });
+
+  //Start scroll functionality 
+  var col1Icons = $('.glyphicon-chevron-up');
+  $('.glyphicon-chevron-up').click(function(){
+    $(".first-column .circle:nth-child(2)").slideUp('fast', function(){
+      $(this).fadeOut('fast');
+    });
+    $(".first-column .circle:nth-child(5)").fadeIn();
+  });
 });
