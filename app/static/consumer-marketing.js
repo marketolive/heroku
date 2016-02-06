928d38c1e9c5f6c6d9e1691b7a50141fac856b66
Chris Pope <cpope13@gmail.com>
2016-02-05 16:33:54 -0800

    Implement column scroll functionality

--- app/static/consumer-marketing.js
+++ app/static/consumer-marketing.js
@@ -9,12 +9,61 @@ $(document).ready(function(){
     $(this).siblings('div').children('img').toggle();
   });
 
-  //Start scroll functionality 
-  var col1Icons = $('.glyphicon-chevron-up');
-  $('.glyphicon-chevron-up').click(function(){
-    $(".first-column .circle:nth-child(2)").slideUp('fast', function(){
-      $(this).fadeOut('fast');
-    });
-    $(".first-column .circle:nth-child(5)").fadeIn();
+  //first column scroll functionality 
+  var iScroll = $('.circle-container-first-column').scrollTop();
+  var myCounter = 0;
+  var grabCircles = $('.circle-container-first-column .circle');
+  var totalCircles = grabCircles.length;
+  var circleHeight = $('.circle').height();
+  $('.glyphicon-chevron-up').css('opacity','0');
+
+  $('.glyphicon-chevron-up:first').click(function(){
+    $('.glyphicon-chevron-down:first').css('opacity','1');
+    if (circleHeight == 125) {
+      if (myCounter > 0) {
+        iScroll -= 175;
+        myCounter -= 1;
+        $('.circle-container-first-column').animate({
+          scrollTop: iScroll
+        }, 600);
+      }
+    } else if (circleHeight == 150) {
+      if (myCounter > 0) {
+        iScroll -= 200;
+        myCounter -= 1;
+        $('.circle-container-first-column').animate({
+          scrollTop: iScroll
+        }, 600);
+      }
+    } 
+    if (myCounter == 0) {
+      $('.glyphicon-chevron-up:first').css('opacity','0');
+    }
   });
+
+
+  $('.glyphicon-chevron-down:first').click(function(){
+    $('.glyphicon-chevron-up:first').css('opacity','1');
+    if (circleHeight == 125) {
+      if ((iScroll >= 0) && (myCounter < (totalCircles - 3))) {
+        iScroll += 175;
+        myCounter += 1;
+        $('.circle-container-first-column').animate({
+          scrollTop: iScroll
+        }, 600); 
+      }
+    } else if (circleHeight == 150) {
+      if ((iScroll >= 0) && (myCounter < (totalCircles - 3))) {
+        iScroll += 200;
+        myCounter +=1;
+        $('.circle-container-first-column').animate({
+          scrollTop: iScroll
+        }, 600);
+      }
+    }
+    if ((myCounter + 3) == totalCircles) {
+      $('.glyphicon-chevron-down:first').css('opacity','0');
+    }
+  });
+
 });
