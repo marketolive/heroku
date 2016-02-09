f0ccb918edf8978e68c514fc99b9f29090e4c3a2
Ben Johnson <bhjohnson@bhjohnson-4819.local>
2016-02-08 17:03:15 -0800

    added deeplinks dynamic population

--- app/static/base.js
+++ app/static/base.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
   $('.help-nav-container').hover(function(){
     $('.help-nav-container').toggle();
   });
+  $.getJSON('/static/deeplinks.json')
+	  .done(function(data){
+	  	console.log("Got JSON");
+	  	$('[deeplink]').each(function(){
+	  		$(this).wrap('<a target="_blank" href="'+data[window.location.pathname.split('/')[1]][$(this).attr('deeplink')]+'"></a>');
+	  	});
+	  })
+	  .fail(function( jqxhr, textStatus, error ) {
+		var err = textStatus + ", " + error;
+		console.log( "Request Failed: " + err );
+	  });
 });
