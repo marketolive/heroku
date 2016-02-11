8727589806bf15a0baa0c4424acb54afc4d87e1e
Chris Pope <cpope13@gmail.com>
2016-02-10 18:09:12 -0800

    updates to email-marketing and customer-base-marketing

---
+++ app/static/icon-toggle.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.circle div').hover(function(){
+    $(this).children('img').toggle();
+  });
+  $('.circle').hover(function(){
+    $(this).children('img').toggle();
+  });
+  $('.circle p').hover(function(){
+    $(this).siblings('div').children('img').toggle();
+  });
+});
