9f092745945667ec911694dabc6e2f6c6e505f99
Ben Johnson <bhjohnson@bhjohnson-4819.local>
2016-06-21 16:12:25 -0700

    updated clone_folder

--- app/utilities.py
+++ app/utilities.py
@@ -1,6 +1,7 @@
 from app.mktorest import MarketoWrapper
 
-from setvars import restClient
+from app.setvars import restClient
+import json
 
 class FailedOut(Exception):
 	def __init__(self, value=None):
@@ -21,7 +22,7 @@ def retry(func, *args, max_tries=None):
 	raise FailedOut()
 
 
-def clone_folder(folder_id, restClient, parent=None, name_append=None):
+def clone_folder(folder_id, restClient, parent=None, name_append='', just_children=False):
 	#Get Parent and name via get folder_by_id
 	try:
 		# folder_detail = retry(restClient.get_folder_by_id, folder_id)
@@ -49,20 +50,20 @@ def clone_folder(folder_id, restClient, parent=None, name_append=None):
 			return False, 'Unable to find folder'
 	#Create new folder in parent
 		print('Creating New Folder')
-		create_result=retry(restClient.create_folder, old_name + ' - clone2', parent)
+		create_result=retry(restClient.create_folder, old_name + name_append, parent)
 		if not create_result['success']:
-			return False, 'Unable to create new base folder'
+			return False, 'Unable to create new base folder: '+json.dumps(create_result['errors'])
 	#recursively clone children (if child = program, clone to new folder, if child = folder, queue for recursive clone)
 		for child in children:
 			print('Cloning Child: %s' % child['name'])
 			if child['folderId']['type']=='Program':
 				#print(child['folderId']['id'], create_result['result'][0]['id'])
-				child_result = retry(restClient.clone_program, child['folderId']['id'], create_result['result'][0]['id'], child['name'] + ' - cloned')
+				child_result = retry(restClient.clone_program, child['folderId']['id'], create_result['result'][0]['id'], child['name'] + name_append)
 				#print(child_result)
 				if not child_result['success']:
 					failures.append({child['folderId']['id']:'Unable to clone'})
 			elif child['folderId']['type']=='Folder':
-				child_success, child_messages = clone_folder(child['folderId']['id'], restClient, parent=create_result['result'][0]['id'])
+				child_success, child_messages = clone_folder(child['folderId']['id'], restClient, parent=create_result['result'][0]['id'], name_append=name_append)
 				if not child_success:
 					failures.append(child_messages)
 		return True, failures
