fa0339584b1e21f2cc103b6a82bfc7ae008eb48f
brianfisher <brian.charles.fisher@gmail.com>
2016-09-01 10:19:13 -0400

    Web Visit Test

--- app/views.py
+++ app/views.py
@@ -251,10 +251,14 @@ def healthcare(language):
 #					API Code Endpoints
 #					
 ########################################################
-@app.route('/en/data/form-fill')
-def form_fill():
+@app.route('/data/form-fill')
+def formFill():
     output = formFill.request()
     return render_template('en/data/form-fill-results.html', content=output)
+    
+@app.route('/data/web-visit')
+def webVisit():
+    return render_template('en/data/web-visit.html')
 
 ########################################################
 #
