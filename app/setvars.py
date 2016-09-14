1b9acc2028831ec5ba6fd636e76684883512a916
brianfisher <brian.charles.fisher@gmail.com>
2016-09-13 20:17:40 -0400

    Change

---
+++ app/setvars.py
@@ -0,0 +1,3 @@
+restcreds = {'munchkin_id':'026-COU-482', 'client_id':'2daff598-1da2-4598-934d-72f53b67a520', 'client_secret':'6T6o09BKCujDxGFXSxBEiN32pVhaH5Au'}
+apiKey = 'UO602DF93nkl'
+FORM_KEY = '193912'
