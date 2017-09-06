8968f0732a186d184035d3e7309fec9eb1c7a56f
brianfisher <brian.charles.fisher@gmail.com>
2017-09-06 12:12:53 -0400

    Support for deleteLead endpoint

--- app/setvars.py
+++ app/setvars.py
@@ -1,3 +1,4 @@
 restcreds = {'munchkin_id':'026-COU-482', 'client_id':'2daff598-1da2-4598-934d-72f53b67a520', 'client_secret':'6T6o09BKCujDxGFXSxBEiN32pVhaH5Au'}
+rest_api_mktosolutions = {'munchkin_id':'226-FBL-320', 'client_id':'76c83615-1636-4cf9-8092-fa75ba51b899', 'client_secret':'GdSY8XEJDVfUDLmAhufIMP1Vxc92oqKr'}
 apiKey = 'UO602DF93nkl'
 FORM_KEY = '193912'
