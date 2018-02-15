b4eafb1227e0c04cfb0e7fc7093eaf244c407e14
Brian Fisher <brian.charles.fisher@gmail.com>
2018-02-15 17:07:50 -0500

    Testing dynamics-lead-web-visit

--- app/static/dynamics-lead-web-visit.js
+++ app/static/dynamics-lead-web-visit.js
@@ -1,4 +1,5 @@
 var mktoLiveMunchkinId = '400-TWP-453',
+referrer = 'https://www.marketolive.com',
 webPages = [
   '/info/community',
   '/info/contact-us',
@@ -24,187 +25,16 @@ webPages = [
   '/reboot-europe-tech-industry.html'
 ],
 leads = [
-  'mktodemosvcs+137@gmail.com',
-  'mktodemosvcs+1529@gmail.com',
-  'mktodemosvcs+1650@gmail.com',
-  'mktodemosvcs+2010@gmail.com',
-  'mktodemosvcs+2658@gmail.com',
-  'mktodemosvcs+2749@gmail.com',
-  'mktodemosvcs+3177@gmail.com',
-  'mktodemosvcs+3331@gmail.com',
-  'mktodemosvcs+3457@gmail.com',
-  'mktodemosvcs+3824@gmail.com',
-  'mktodemosvcs+3851@gmail.com',
-  'mktodemosvcs+3959@gmail.com',
-  'mktodemosvcs+4377@gmail.com',
-  'mktodemosvcs+4525@gmail.com',
-  'mktodemosvcs+4644@gmail.com',
-  'mktodemosvcs+4690@gmail.com',
-  'mktodemosvcs+4792@gmail.com',
-  'mktodemosvcs+5197@gmail.com',
-  'mktodemosvcs+5490@gmail.com',
-  'mktodemosvcs+5668@gmail.com',
-  'mktodemosvcs+5863@gmail.com',
-  'mktodemosvcs+5982@gmail.com',
-  'mktodemosvcs+6040@gmail.com',
-  'mktodemosvcs+6264@gmail.com',
-  'mktodemosvcs+6612@gmail.com',
-  'mktodemosvcs+6859@gmail.com',
-  'mktodemosvcs+7307@gmail.com',
-  'mktodemosvcs+7381@gmail.com',
-  'mktodemosvcs+7385@gmail.com',
-  'mktodemosvcs+7445@gmail.com',
-  'mktodemosvcs+7454@gmail.com',
-  'mktodemosvcs+7640@gmail.com',
-  'mktodemosvcs+7802@gmail.com',
-  'mktodemosvcs+7915@gmail.com',
-  'mktodemosvcs+8102@gmail.com',
-  'mktodemosvcs+8321@gmail.com',
-  'mktodemosvcs+8482@gmail.com',
-  'mktodemosvcs+8572@gmail.com',
-  'mktodemosvcs+8895@gmail.com',
-  'mktodemosvcs+91@gmail.com',
-  'mktodemosvcs+9170@gmail.com',
-  'mktodemosvcs+9229@gmail.com',
-  'mktodemosvcs+9523@gmail.com',
-  'mktodemosvcs+990@gmail.com',
-  'mktodemosvcs+abuckles@gmail.com',
-  'mktodemosvcs+achamfeuil@gmail.com',
-  'mktodemosvcs+agorohovski@gmail.com',
-  'mktodemosvcs+akaufmann@gmail.com',
-  'mktodemosvcs+akomori@gmail.com',
-  'mktodemosvcs+arogulj@gmail.com',
-  'mktodemosvcs+batwater@gmail.com',
   'mktodemosvcs+bburne@gmail.com',
   'mktodemosvcs+bfisher@gmail.com',
-  'mktodemosvcs+bnukala@gmail.com',
-  'mktodemosvcs+cakennedy@gmail.com',
-  'mktodemosvcs+caryl_mostacho@gmail.com',
-  'mktodemosvcs+cathal@gmail.com',
-  'mktodemosvcs+cburgess@gmail.com',
-  'mktodemosvcs+ccaldwell@gmail.com',
-  'mktodemosvcs+cchao@gmail.com',
-  'mktodemosvcs+ccrandall@gmail.com',
-  'mktodemosvcs+ccurran@gmail.com',
-  'mktodemosvcs+christianf@gmail.com',
-  'mktodemosvcs+civey@gmail.com',
-  'mktodemosvcs+cleroy@gmail.com',
-  'mktodemosvcs+cmeyer@gmail.com',
-  'mktodemosvcs+ctiernan@gmail.com',
-  'mktodemosvcs+ctriantafillou@gmail.com',
-  'mktodemosvcs+dconahan@gmail.com',
-  'mktodemosvcs+ddehart@gmail.com',
-  'mktodemosvcs+ddickinson@gmail.com',
-  'mktodemosvcs+dli@gmail.com',
-  'mktodemosvcs+dliscia@gmail.com',
-  'mktodemosvcs+dmarshall@gmail.com',
-  'mktodemosvcs+dmorales@gmail.com',
-  'mktodemosvcs+dmyers@gmail.com',
-  'mktodemosvcs+dongyoung_jung@gmail.com',
-  'mktodemosvcs+dorr@gmail.com',
-  'mktodemosvcs+dsanders@gmail.com',
-  'mktodemosvcs+ebagnall@gmail.com',
-  'mktodemosvcs+egraziano@gmail.com',
-  'mktodemosvcs+eramirez@gmail.com',
-  'mktodemosvcs+ewilliams@gmail.com',
-  'mktodemosvcs+fknox@gmail.com',
-  'mktodemosvcs+fpassantino@gmail.com',
-  'mktodemosvcs+gcruz@gmail.com',
-  'mktodemosvcs+gmanlongat@gmail.com',
-  'mktodemosvcs+gmurfee@gmail.com',
-  'mktodemosvcs+gryan@gmail.com',
   'mktodemosvcs+hgrant@gmail.com',
-  'mktodemosvcs+hmerritt@gmail.com',
-  'mktodemosvcs+hmitsuhashi@gmail.com',
-  'mktodemosvcs+hparameswaran@gmail.com',
-  'mktodemosvcs+hperroquin@gmail.com',
-  'mktodemosvcs+jboush@gmail.com',
-  'mktodemosvcs+jchang@gmail.com',
-  'mktodemosvcs+jcollazo@gmail.com',
-  'mktodemosvcs+jfrickle@gmail.com',
-  'mktodemosvcs+jhanafusa@gmail.com',
-  'mktodemosvcs+jhayasaka@gmail.com',
-  'mktodemosvcs+jkurokawa@gmail.com',
   'mktodemosvcs+jlangensand@gmail.com',
-  'mktodemosvcs+jlewis@gmail.com',
   'mktodemosvcs+jmcpherson@gmail.com',
-  'mktodemosvcs+jmenden@gmail.com',
-  'mktodemosvcs+jnierodzik@gmail.com',
-  'mktodemosvcs+jnorris@gmail.com',
-  'mktodemosvcs+jsutachan@gmail.com',
-  'mktodemosvcs+kbruno@gmail.com',
-  'mktodemosvcs+kdeshpande@gmail.com',
-  'mktodemosvcs+kgoto@gmail.com',
-  'mktodemosvcs+kmccoy@gmail.com',
-  'mktodemosvcs+ktakeuchi@gmail.com',
-  'mktodemosvcs+ktan@gmail.com',
-  'mktodemosvcs+ldefreitas@gmail.com',
   'mktodemosvcs+lmcrae@gmail.com',
-  'mktodemosvcs+lmolex@gmail.com',
-  'mktodemosvcs+lmorschhauser@gmail.com',
-  'mktodemosvcs+lunger@gmail.com',
-  'mktodemosvcs+mbrown@gmail.com',
-  'mktodemosvcs+mduvall@gmail.com',
   'mktodemosvcs+mfazio@gmail.com',
-  'mktodemosvcs+mgokoffski@gmail.com',
-  'mktodemosvcs+mhandes@gmail.com',
-  'mktodemosvcs+mhaviv@gmail.com',
-  'mktodemosvcs+michaelm@gmail.com',
-  'mktodemosvcs+mikem@gmail.com',
-  'mktodemosvcs+mkirschner@gmail.com',
-  'mktodemosvcs+mkuhlman@gmail.com',
-  'mktodemosvcs+mlewis@gmail.com',
-  'mktodemosvcs+mmadden@gmail.com',
-  'mktodemosvcs+mparmer@gmail.com',
-  'mktodemosvcs+navaro@gmail.com',
-  'mktodemosvcs+ndennis@gmail.com',
   'mktodemosvcs+omalik@gmail.com',
-  'mktodemosvcs+pahudson@gmail.com',
-  'mktodemosvcs+pdellecase@gmail.com',
-  'mktodemosvcs+ralfieri@gmail.com',
-  'mktodemosvcs+rbanaag@gmail.com',
-  'mktodemosvcs+rlaib@gmail.com',
-  'mktodemosvcs+rlanders@gmail.com',
-  'mktodemosvcs+rmartin@gmail.com',
-  'mktodemosvcs+rmorgan@gmail.com',
-  'mktodemosvcs+ryu@gmail.com',
-  'mktodemosvcs+sabudagen@gmail.com',
-  'mktodemosvcs+sallen@gmail.com',
-  'mktodemosvcs+satoshi_yamashita@gmail.com',
-  'mktodemosvcs+scallahan@gmail.com',
-  'mktodemosvcs+schiba@gmail.com',
-  'mktodemosvcs+scottm@gmail.com',
-  'mktodemosvcs+shardie@gmail.com',
-  'mktodemosvcs+simon@gmail.com',
-  'mktodemosvcs+skalra@gmail.com',
-  'mktodemosvcs+smccullen@gmail.com',
-  'mktodemosvcs+smcray@gmail.com',
-  'mktodemosvcs+smutairi@gmail.com',
-  'mktodemosvcs+sohne@gmail.com',
-  'mktodemosvcs+srsharma@gmail.com',
-  'mktodemosvcs+ssakadales@gmail.com',
-  'mktodemosvcs+ssauter@gmail.com',
-  'mktodemosvcs+stsuchiyama@gmail.com',
-  'mktodemosvcs+tbeppu@gmail.com',
   'mktodemosvcs+tcarroll@gmail.com',
-  'mktodemosvcs+tconnor@gmail.com',
-  'mktodemosvcs+tebradley@gmail.com',
-  'mktodemosvcs+thadley@gmail.com',
-  'mktodemosvcs+thironaka@gmail.com',
-  'mktodemosvcs+tsullivan@gmail.com',
-  'mktodemosvcs+ttorres@gmail.com',
-  'mktodemosvcs+uottman@gmail.com',
-  'mktodemosvcs+vokere@gmail.com',
-  'mktodemosvcs+wdurnin@gmail.com',
-  'mktodemosvcs+yhirokawa@gmail.com',
-  'mktodemosvcs+ykobayashi@gmail.com',
-  'mktodemosvcs+yshirai@gmail.com',
-  'mktodemosvcs+ytakanohashi@gmail.com',
-  'mktodemosvcs+ytanaka@gmail.com',
-  'mktodemosvcs+yyuhara@gmail.com',
-  'mktodemosvcs+zholmes@gmail.com',
-  'mktodemosvcs+zmackinnon@gmail.com',
-  'mktodemosvcs+zwatson@gmail.com'
+  'mktodemosvcs+tconnor@gmail.com'
 ];
 
 (function () {
@@ -229,6 +59,18 @@ leads = [
     }
     console.log('Getting > Cookie: ' + cookieName + ' not found');
     return null;
+  }
+  
+  function overloadMunchkinCreateTrackingCookie() {
+    if (typeof(origMunchkinCreateTrackingCookie) !== 'function') {
+      origMunchkinCreateTrackingCookie = Munchkin.createTrackingCookie;
+    }
+    
+    Munchkin.createTrackingCookie = function (b, callback) {
+      origMunchkinCreateTrackingCookie.apply(this, arguments);
+      console.log('Completed > Create Tracking Cookie');
+      callback();
+    };
   }
   
   function overloadMunchkinInit() {
@@ -310,23 +152,24 @@ leads = [
   }
   
   function submitLeadData() {
-    var acmeLeadX = leads[Math.floor(Math.random() * leads.length)];
+    var leadX = leads[Math.floor(Math.random() * leads.length)];
     
-    if (acmeLeadX) {
+    if (leadX) {
       resetMunchkinCookie(mktoLiveMunchkinId, function () {
         window.setTimeout(function () {
-          console.log('Associating > Mock Lead: ' + acmeLeadX);
-          
+          console.log('Associating > Mock Lead: ' + leadX);
+          debugger;
           overloadMunchkinFunction();
           Munchkin.munchkinFunction('associateLead', {
-            Email: acmeLeadX
-          }, sha1('123123123' + acmeLeadX), function () {
+            Email: leadX
+          }, sha1('123123123' + leadX), function () {
             var webPageX = webPages[Math.floor(Math.random() * webPages.length)];
-            console.log('Posting > Mock Lead > Visit Web Page: ' + acmeLeadX + ' : ' + webPageX);
+            console.log('Posting > Mock Lead > Visit Web Page: ' + leadX + ' : ' + webPageX);
             
             overloadMunchkinFunction();
             Munchkin.munchkinFunction('visitWebPage', {
-              url: webPageX
+              url: webPageX,
+              referrer: referrer
             }, null, function () {
               resetMasterMunchkinCookie(function () {
                 window.setTimeout(function () {
