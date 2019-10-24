fccd1d6224800231a112814a17a2de399bd3102b
Hunter Grant <huntergrant@gmail.com>
2019-10-24 12:13:42 -0400

    Updated search API key

--- app/static/ad-targeting.js
+++ app/static/ad-targeting.js
@@ -2,7 +2,7 @@ var devExtensionId = "dokkjhbgengdlccldgjnbilajdbjlnhm",
 prodExtensionId = "onibnnoghllldiecboelbpcaeggfiohl",
 extensionId = devExtensionId,
 
-key = "AIzaSyC9pdVq6GfquP_MtHCS_izS6Vijdv1ZfNc",
+key = "AIzaSyAvAXVyU31d09qQczySNunmizsJtSgq89w",
 cx = "014680826408884315290:pmyltjjihus",
 startIndex = 1,
 
